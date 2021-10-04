80e765f4b583eebcc3570fe5a2b3641a4a4b0061
shafik-soweb <shafiksoweb191@gmail.com>
2021-10-04 10:25:32 +0600

    first commit

---
+++ javascript.js
@@ -0,0 +1,50 @@
+/*const calcTip = function(bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+const bills = [125, 555, 54];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills, tips,totals);
+
+const mark = {
+    fullName: `Mark Miller`,
+    mass: 78,
+    height: 1.69,
+    calcBMI: function () {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
+    }
+
+};
+const john = {
+    fullName: `John Smith`,
+    mass: 92,
+    height: 1.95,
+    calcBMI: function () {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
+    }
+};
+mark.calcBMI();
+john.calcBMI();
+console.log(mark.bmi, john.bmi);
+if (mark.bmi > john.bmi) {
+    console.log(`${mark.fullName}'s BMI (${ mark.bmi }) is heigher than ${john.fullName }'s BMI(${ john.bmi})`)
+}
+
+for (let rep = 1; rep <= 11; rep++) {
+    console.log(`Weight liftting repetition ${rep}`);
+}*/
+const jonas = [
+    `JOnas`,
+    `Tomas`,
+    teacher,
+    2037 - 1991,
+    [`Michel`, `Jerry`, `Peter`],
+    true
+]
+
+
+
+
+
